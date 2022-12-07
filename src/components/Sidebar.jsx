5279bf109294a9b45b2f54573b9dba68bcdf8e19
Gideon Addo <gideonaddoofori@gmail.com>
2022-12-07 16:50:28 +0000

    Update value of attribute 'role'

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
 
   return (
     <nav aria-label='Sidebar' onClick={(e) => e.target.classList.contains('fixed') && setIsSidebarOpen(false)} className={`fixed ${isSidebarOpen ? 'visible translate-x-0' : 'invisible translate-x-full'} top-0 right-0 w-screen overflow-hidden h-screen z-10 text-offWhite font-barlowCondensed transition-all duration-500`}>
-      <button role='close primary navigation' aria-controls='primary-navigation' onClick={hideSidebar} className='absolute top-8 right-8 z-20 active:scale-95 transition-transform'>
+      <button aria-controls='primary-navigation' onClick={hideSidebar} className='absolute top-8 right-8 z-20 active:scale-95 transition-transform'>
         <img src={iconClose} alt="close sidebar icon" />
       </button>
       <ul id='primary-navigation' className='bg-black/95 tracking-widest font-light text-lg uppercase supports-[backdrop-filter]:bg-white/[0.04] supports-[backdrop-filter]:backdrop-blur-2xl h-full w-4/6 absolute right-0 top-0 px-8 space-y-7 pt-28'>
